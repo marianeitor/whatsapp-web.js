377aaf1d557bae9833406ad2ece13afc31f1cf67
Mariano <mariano1colombo@gmail.com>
2022-11-19 00:31:48 -0300

    Added plus

--- src/Client.js
+++ src/Client.js
@@ -1043,7 +1043,7 @@ class Client extends EventEmitter {
 
         return await this.pupPage.evaluate(async number => {
             //const wid = window.Store.WidFactory.createWid(number);
-            const result = await window.Store.QueryExist("phone","+"number);
+            const result = await window.Store.QueryExist("phone","+" + number);
             if (!result || result.wid === undefined) return null;
             return result.wid;
         }, number);
